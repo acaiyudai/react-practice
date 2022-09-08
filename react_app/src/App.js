936e2e8b61e822ed9cbcd9cb7dbca46fb6fc5f0e
acaiyudai <ispectrene@outlook.com>
2022-09-08 18:47:10 +0900

    Component:use Context

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,77 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
 
-/*-----------------------------------------
-オリジナルの属性'onCheck'を使うための処理.
-1 onCheck時に実行される関数'doCheck'を作る.
-2 入力文字数の最大値'10'と'onCheck'属性を, Messageインスタンスに渡す.
-3 Messageクラス内で, onChange時に実行される関数'doChange'を作る.
-  doChangeは文字が入力されるたびに実行される.
-  入力された文字数と最大値を比較する.
-  最大値より大きければ, onCheck属性の関数'doCheck'を実行する.
+/*---------------------------------------------------------------
+コンポーネント間で共通の変数'SampleContext'を利用できる機能'Context'.
+-----------------------------------------------------------------*/
 
--------------------------------------------*/
+let data = {
+  title:'title',
+  message:'this is sample message.'
+};
+
+const SampleContext = React.createContext(data);
 
 class App extends Component {
-  /*-- field --*/
-  input = '';
-
-  msgStyle = {
-    fontSize:'20pt',
-    color:'#900',
-    margin:'10px 0px',
-    padding:'5px'
+  render(){
+    return (
+      <div>
+        <h1>Context</h1>
+        <Title />
+        <Message />
+      </div>
+    );
   }
+}
 
-  /*-- method --*/
-  constructor(props){
-    super(props);
-    this.state = {
-      message:'type your name.'
-    };
-    this.doCheck = this.doCheck.bind(this);
-  }
-
-  doCheck(event){
-    alert(event.target.value + 'は長すぎます(最大10文字).')
-  }
+class Title extends Component {
+  static contextType = SampleContext;
 
   render(){
     return (
       <div>
-        <h1>React</h1>
-        <h2>{this.state.message}</h2>
-        <Message maxlength='10' onCheck={this.doCheck} />
+        <h2>{this.context.title}</h2>
       </div>
     );
   }
-
 }
 
 class Message extends Component {
-  /*-- field --*/
-  inputStyle = {
-    fontSize:'12pt',
-    padding:'5px'
-  }
-
-  /*-- method --*/
-  constructor(props){
-    super(props);
-    this.doChange = this.doChange.bind(this);
-  }
-
-  doChange(e){
-    if (e.target.value.length > this.props.maxlength){
-      this.props.onCheck(e);
-      //指定した位置から, 指定した文字数分の文字列を抜き出して返す関数'substr'
-      e.target.value = e.target.value.substr(0, this.props.maxlength);
-    }
-  }
+  static contextType = SampleContext;
 
   render(){
     return (
-      <input type='text' style={this.inputStyle} onChange={this.doChange} />
+      <div>
+        <p>{this.context.message}</p>
+      </div>
     );
   }
 }
