fee859cd4c07575350bddf0bffec8741ccaa1b48
acaiyudai <ispectrene@outlook.com>
2022-09-08 18:32:20 +0900

    Component:input name with limited text size by using original field(onCheck)

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
+/*-----------------------------------------
+オリジナルの属性'onCheck'を使うための処理.
+1 onCheck時に実行される関数'doCheck'を作る.
+2 入力文字数の最大値'10'と'onCheck'属性を, Messageインスタンスに渡す.
+3 Messageクラス内で, onChange時に実行される関数'doChange'を作る.
+  doChangeは文字が入力されるたびに実行される.
+  入力された文字数と最大値を比較する.
+  最大値より大きければ, onCheck属性の関数'doCheck'を実行する.
+
+-------------------------------------------*/
+
 class App extends Component {
   /*-- field --*/
   input = '';
@@ -12,6 +23,33 @@ class App extends Component {
     padding:'5px'
   }
 
+  /*-- method --*/
+  constructor(props){
+    super(props);
+    this.state = {
+      message:'type your name.'
+    };
+    this.doCheck = this.doCheck.bind(this);
+  }
+
+  doCheck(event){
+    alert(event.target.value + 'は長すぎます(最大10文字).')
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>React</h1>
+        <h2>{this.state.message}</h2>
+        <Message maxlength='10' onCheck={this.doCheck} />
+      </div>
+    );
+  }
+
+}
+
+class Message extends Component {
+  /*-- field --*/
   inputStyle = {
     fontSize:'12pt',
     padding:'5px'
@@ -20,43 +58,22 @@ class App extends Component {
   /*-- method --*/
   constructor(props){
     super(props);
-    this.state = {
-      message:'type your name.'
-    };
     this.doChange = this.doChange.bind(this);
-    this.doSubmit = this.doSubmit.bind(this);
   }
 
-  doChange(event){
-    this.input = event.target.value;
-  }
-
-  doSubmit(event){
-    this.setState({
-      message:'Hello, ' + this.input + '!'
-    });
-    event.preventDefault();
+  doChange(e){
+    if (e.target.value.length > this.props.maxlength){
+      this.props.onCheck(e);
+      //指定した位置から, 指定した文字数分の文字列を抜き出して返す関数'substr'
+      e.target.value = e.target.value.substr(0, this.props.maxlength);
+    }
   }
 
   render(){
     return (
-      <div>
-        <h1>React</h1>
-        <h2>{this.state.message}</h2>
-        <form onSubmit={this.doSubmit}>
-          <label>
-            <span style={this.inputStyle}></span>Message:
-            <input type='text' style={this.inputStyle} 
-             onChange={this.doChange} 
-             //required pattern='[A-Za-z _,.]+' //(入力の表記方法に制限をかける場合はコメントを外す)
-             />
-          </label>
-          <input type='submit' style={this.inputStyle} value='Click' />
-        </form>
-      </div>
+      <input type='text' style={this.inputStyle} onChange={this.doChange} />
     );
   }
-
 }
 
 export default App;
