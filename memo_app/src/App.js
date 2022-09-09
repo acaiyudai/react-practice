5657ce4eab5609d91dc66379dfcc1951cb947e89
acaiyudai <ispectrene@outlook.com>
2022-09-09 16:15:53 +0900

    Add : setting of index.js and App.js

--- memo_app/src/App.js
+++ memo_app/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Memo from './memo/Memo'
+import AddForm from './memo/AddForm';
+import FindForm from './memo/FindForm';
+import DelForm from './memo/DelForm';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// App component
+class App extends Component {
+  /*-- field --*/
+  td = {
+    width:'250px'
+  }
+  /*-- method --*/
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>RedVelvet</h1>
+        <AddForm />
+        <hr />
+        <table><tbody><tr>
+          <td style={this.td}><FindForm /></td>
+          <td style={this.td}><DelForm /></td>
+        </tr></tbody></table>
+        <Memo />
+      </div>
+    );
+  }
+
 }
 
 export default App;
