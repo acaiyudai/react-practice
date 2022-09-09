6c2f901078d17719ea3a71c8db6188423792f8bc
acaiyudai <ispectrene@outlook.com>
2022-09-10 01:27:26 +0900

    lesson : useState

--- react-basic/src/features/Article.jsx
+++ react-basic/src/features/Article.jsx
@@ -1,8 +1,19 @@
+import {useState} from 'react';
+import PublishButton from './PublishButton';
+
 const Article = (props) => {
+    const [isPublished, setIsPublished] = useState(false);
+    console.log(isPublished);
+    const publishArticle = () => {
+        setIsPublished(true);
+    }
+
+
     return (
         <div>
             <h2>{props.title}</h2>
             <p>{props.content}</p>
+            <PublishButton isPublished={isPublished} onClick={() => publishArticle()} />
         </div>
     );
 }
